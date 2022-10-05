a8dc90f3a61b841624f5993c732a03846de01c25
Nataliia <natali_94@ukr.net>
2022-10-05 13:15:50 +0300

    homework

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+  let elem = document.querySelectorAll('li');
 
-export const findElement = () => {};
+  for (let index = 0; index < elem.length; index++) {
+    if (index % 2 === 0) {
+      elem[index].style.backgroundColor = 'red';
+    }
+  }
+
+};
+
+export const findElement = () => {
+  let elem = document.querySelector('li');
+  let elements = document.querySelectorAll('li');
+
+  for (let index = 0; index < elements.length; index++) {
+    if (elem.matches('.likedItem')) {
+      elem.style.backgroundColor = 'blue';
+    }
+    else {
+      elem = elem.nextElementSibling;
+    }
+  }
+};
